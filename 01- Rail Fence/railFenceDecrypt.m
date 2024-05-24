dffb972fa80fef1e2116c5e11b4379d90b83244b
mohamedhassan218 <m.hassan.def@gmail.com>
2024-05-24 17:08:11 +0300

    Explain the Rail Fence Cypher with detailed comments.

--- 01- Rail Fence/railFenceDecrypt.m
+++ 01- Rail Fence/railFenceDecrypt.m
@@ -1,25 +1,39 @@
 function plainText = railFenceDecrypt(cipherText, rails)
-    % Preallocate the plainText variable
+    %{
+        Initialize the plainText variable (explained in details in the encryption).
+    %}
     plainText = repmat(' ', 1, length(cipherText));
-    
-    % Calculate the number of characters in each rail
+
+    %{
+        This block calculates the number of characters in each rail.
+        It first calculates the number of characters in the first and last rails,
+            which are the same. Then calculates the number of characters in the rails in between.
+    %}
     charsInRail = zeros(1, rails);
     charsInRail(1) = ceil(length(cipherText) / (2 * rails - 2));
-    for i = 2:rails-1
+    for i = 2:rails - 1
         charsInRail(i) = 2 * charsInRail(1);
     end
     charsInRail(rails) = charsInRail(1);
-    
-    % Initialize variables
+
+    % The same purpose as in the encryption part.
     idx = 1;
     direction = 1;
-    
-    % Loop through each rail
+
+    % Begin the decryption proces . . .
     for row = 1:rails
+        % Set the starting position for the current rail.
         i = row;
+
+        % Loop to iterate over the characters in the cipherText that belong to the current rail.
         while i <= length(cipherText) && idx <= length(cipherText)
+            % Add the current character from the cipherText to the plainText at the current position.
             plainText(i) = cipherText(idx);
+
+            % Move to the next position in the cypherText.
             idx = idx + 1;
+
+            % Determine the next position (above or below) using direction variable.
             if row == 1 || row == rails
                 i = i + (rails - 1) * 2;
             elseif direction == 1
@@ -29,7 +43,11 @@ function plainText = railFenceDecrypt(cipherText, rails)
                 i = i + (row - 1) * 2;
                 direction = 1;
             end
+
         end
+
+        % Reset our direction variable to begin the next rail.
         direction = 1;
     end
+
 end
--- 01- Rail Fence/railFenceEncrypt.m
+++ 01- Rail Fence/railFenceEncrypt.m
@@ -1,20 +1,47 @@
+%{
+    Define a function that takes two arguments: plaintext and rails.
+    It also returns the ciphertext.
+%}
 function cipherText = railFenceEncrypt(plainText, rails)
-    % Remove spaces from plaintext
+    %{
+        Remove all spaces from our plaintext.
+        strrep(): replace all occurrences of a specified character with another character.
+    %}
     plainText = strrep(plainText, ' ', '');
 
-    % Preallocate the cipherText variable
+    %{
+        This line aims to initialize the cipherText variable.
+        repmat(): stands for repeat matrix and it's job to repeat new matrix using placed one.
+        The single space character means that the function will replicate a space character
+            to create the initial value of cipherText times the length of the plainText.
+        The '1' specifies the number of times the space character will be repeated along
+            the row dimension.
+    %}
     cipherText = repmat(' ', 1, length(plainText));
-    
-    % Initialize variables
+
+    %{
+        idx: to keep track of the current position in the cipherText.
+        direction: to determine wheater the next character should be add to the cipherText
+            above or below the current position.
+    %}
     idx = 1;
     direction = 1;
-    
-    % Loop through each rail
+
+    % Begin the encryption process . . .
     for row = 1:rails
         i = row;
+        % The while iterates over the characters in the plainText that belong
+        %    to the current rail.
         while i <= length(plainText)
+            % Add the current character from the plainText to the cipherText
+            %   at the current position.
             cipherText(idx) = plainText(i);
+
+            % Move to the next position in the cipherText array.
             idx = idx + 1;
+
+            % This block determines the next position in the plainText array using
+            %   the direction variable.
             if row == 1 || row == rails
                 i = i + (rails - 1) * 2;
             elseif direction == 1
@@ -24,7 +51,11 @@ function cipherText = railFenceEncrypt(plainText, rails)
                 i = i + (row - 1) * 2;
                 direction = 1;
             end
+
         end
+
+        % Reset our direction variable to be ready to the next rail.
         direction = 1;
     end
+
 end
