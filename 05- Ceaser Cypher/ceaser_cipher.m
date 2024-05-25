95ead1a0a28a5c95a446fafeeaaf9b159087e15d
mohamedhassan218 <m.hassan.def@gmail.com>
2024-05-25 07:35:52 +0300

    Study the Ceaser Cipher and add comments successfully.

--- 05- Ceaser Cypher/ceaser_cipher.m
+++ 05- Ceaser Cypher/ceaser_cipher.m
@@ -1,13 +1,23 @@
-function[cipher_text]=ceaser_cipher(plain_text,key)
-    chars='a':'z';
-    plain_text=lower(plain_text);
-    plain_text=plain_text(plain_text~=' ');
-    cipher_text='';
-    for i =1:length(plain_text)
-        cipheridx=find(chars==plain_text(i));
-        cipheridx=cipheridx+key;
-        cipheridx=mod((cipheridx-1),26)+1;
-        cipher_text(i)=chars(cipheridx);
+function [cipher_text] = ceaser_cipher(plain_text, key)
+    % chars = "abcdefghijklmnopqrstuvwxyz".
+    chars = 'a':'z';
+
+    % Convert to lowercase.
+    plain_text = lower(plain_text);
+
+    % Remove spaces.
+    plain_text = plain_text(plain_text ~= ' ');
+
+    % Empty string to hold our encrypted_text.
+    cipher_text = '';
+
+    % Encryption begins . . .
+    for i = 1:length(plain_text)
+        cipheridx = find(chars == plain_text(i));
+        cipheridx = cipheridx + key;
+        cipheridx = mod((cipheridx - 1), 26) + 1;
+        cipher_text(i) = chars(cipheridx);
     end
-    cipher_text=upper(cipher_text);
+
+    cipher_text = upper(cipher_text);
 end
--- 05- Ceaser Cypher/decrypt_ceaser_cipher.m
+++ 05- Ceaser Cypher/decrypt_ceaser_cipher.m
@@ -1,11 +1,19 @@
-function[decrypted_text] = decrypt_ceaser_cipher(cipher_text, key)
-    chars='a':'z';
-    cipher_text=lower(cipher_text);
-    decrypted_text='';
-    for i =1:length(cipher_text)
-        cipheridx=find(chars==cipher_text(i));
-        cipheridx=cipheridx-key;
-        cipheridx=mod((cipheridx-1),26)+1;
-        decrypted_text(i)=chars(cipheridx);
+function [decrypted_text] = decrypt_ceaser_cipher(cipher_text, key)
+    % chars = "abcdefghijklmnopqrstuvwxyz".
+    chars = 'a':'z';
+
+    % Convert to lower.
+    cipher_text = lower(cipher_text);
+
+    % Empty string that will be our decrypted_text.
+    decrypted_text = '';
+
+    % Decryption happens . . .
+    for i = 1:length(cipher_text)
+        cipheridx = find(chars == cipher_text(i));
+        cipheridx = cipheridx - key;
+        cipheridx = mod((cipheridx - 1), 26) + 1;
+        decrypted_text(i) = chars(cipheridx);
     end
+
 end
