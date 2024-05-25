563a90b0deeafd7324cd51eddc20d63b2539f981
mohamedhassan218 <m.hassan.def@gmail.com>
2024-05-25 07:47:08 +0300

    Explain the Double Transposition cipher with detailed comments.

--- 03- Double Transposition/double_transposition_decrypt.m
+++ 03- Double Transposition/double_transposition_decrypt.m
@@ -1,29 +1,52 @@
-function Plain_txt = double_transposition_decrypt(Cipher_txt,Row_Per,Col_Per)
+function Plain_txt = double_transposition_decrypt(Cipher_txt, Row_Per, Col_Per)
+    % Convert to lowercase.
     Cipher_txt = lower(Cipher_txt);
-    Cipher_txt = Cipher_txt( Cipher_txt ~= ' ' );
+
+    % Remove white spaces.
+    Cipher_txt = Cipher_txt(Cipher_txt ~= ' ');
+
+    % Initilize the plain_text (our result) with the value of the cipher_text.
     Plain_txt = Cipher_txt;
+
+    % Grap the row and column values using the length of the row_per and col_per.
     row = length(Row_Per);
     col = length(Col_Per);
+
+    % Counter to help us.
     C = 1;
+
+    % Fill our matrix with the cipher_text.
     for I = 1:row
+
         for J = 1:col
+
             if C <= length(Cipher_txt)
-                Cipher_Mat(I,J) = Cipher_txt(C);
+                Cipher_Mat(I, J) = Cipher_txt(C);
             else
-                Cipher_Mat(I,J) = 'x';
+                Cipher_Mat(I, J) = 'x';
             end
+
             C = C + 1;
         end
+
     end
-    for I = 1:col    
-        Permuted_Mat1(:,Col_Per(I)) = Cipher_Mat(:,I); 
+
+    % Let's decrypt . . .
+    for I = 1:col
+        Permuted_Mat1(:, Col_Per(I)) = Cipher_Mat(:, I);
     end
-    for I = 1:row                 
-        Permuted_Mat2(Row_Per(I),:)= Permuted_Mat1(I,:); 
+
+    for I = 1:row
+        Permuted_Mat2(Row_Per(I), :) = Permuted_Mat1(I, :);
     end
+
+    % And don't forget to transpose your permuted matrix.
     Permuted_Mat2 = Permuted_Mat2';
-    for I = 1:row*col
+
+    % Now we've to convert the matrix to a string again.
+    for I = 1:row * col
         Permuted_Mat2(I)
         Plain_txt(I) = Permuted_Mat2(I);
     end
+
 end
--- 03- Double Transposition/double_transposition_encrypt.m
+++ 03- Double Transposition/double_transposition_encrypt.m
@@ -1,34 +1,56 @@
-function Cipher_txt = double_transposition_encrypt(Plain_txt,Row_Per,Col_Per)
+function Cipher_txt = double_transposition_encrypt(Plain_txt, Row_Per, Col_Per)
+    % Convert the plain text to lower case.
     Plain_txt = lower(Plain_txt);
-    Plain_txt = Plain_txt( Plain_txt ~= ' ' );
+
+    % Remove spaces from the plain text.
+    Plain_txt = Plain_txt(Plain_txt ~= ' ');
+
+    % Initialize the cipher text with the plain text.
     Cipher_txt = Plain_txt;
 
+    % Get the number of rows and columns from the permutation arrays.
     row = length(Row_Per);
     col = length(Col_Per);
+
+    % Initialize a counter for the plain text.
     C = 1;
+
+    % Fill the plain text matrix with the plain text characters.
     for I = 1:row
+
         for J = 1:col
+            % If there are still characters in the plain text, use them.
             if C <= length(Plain_txt)
-                Plain_Mat(I,J) = Plain_txt(C);
+                Plain_Mat(I, J) = Plain_txt(C);
             else
-                Plain_Mat(I,J) = 'x';
+                % If there are no more characters, fill the rest of the matrix with 'x'.
+                Plain_Mat(I, J) = 'x';
             end
+
+            % Increment the counter.
             C = C + 1;
         end
-    end
-    for I = 1:row
-        Permuted_Mat1(I,:)= Plain_Mat(Row_Per(I),:);    
-    end
-    for I = 1:col
-        Permuted_Mat2(:,I) = Permuted_Mat1(:,Col_Per(I));    
+
     end
 
+    % Permute the rows of the plain text matrix.
+    for I = 1:row
+        Permuted_Mat1(I, :) = Plain_Mat(Row_Per(I), :);
+    end
+
+    % Permute the columns of the permuted matrix.
+    for I = 1:col
+        Permuted_Mat2(:, I) = Permuted_Mat1(:, Col_Per(I));
+    end
+
+    % Transpose the permuted matrix (notice the single quote).
     Permuted_Mat2 = Permuted_Mat2';
-    for I = 1:row*col
+
+    % Convert the permuted matrix back to a cipher text.
+    for I = 1:row * col
         Cipher_txt(I) = Permuted_Mat2(I);
     end
 
+    % Convert the cipher text to upper case.
     Cipher_txt = upper(Cipher_txt);
 end
-% Cipher_txt=Dsub_Encrpt('helloworld',[3 5 1 4 2],[2 1])
-% Cipher_txt=WODLEHROLL
